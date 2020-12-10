592e827c73f940f28d1a2d7b16b11ea25ba299ee
DESKTOP-ENBUIB3\cabeard21 <cabeard21@gmail.com>
2020-12-10 01:48:17 -0600

    Refactor AoBinUtils class. Implemented singleton.

--- My_Utilities/ao_bin_dumps_utilities.py
+++ My_Utilities/ao_bin_dumps_utilities.py
@@ -2,36 +2,43 @@ import json
 import os  # For relative paths
 
 
-class AoBinUtils():
+class SingletonMeta(type):
 
-    _items = []
-    _names = []
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+
+        return cls._instances[cls]
+
+
+class AoBinUtils(metaclass=SingletonMeta):
+
     _item_name = {}
 
-    def __init__(self, *args, **kwargs):
-        fp_items = kwargs.get('item_file', None)
-        fp_names = kwargs.get('name_file', None)
-
+    def __init__(self, item_file='..\\items.json', name_file='..\\formatted\\items.json'):
         try:
-            assert fp_items is not None, "Item file not supplied"
-            assert fp_names is not None, "Name file not supplied"
+            fp_items = os.path.join(os.path.dirname(__file__), item_file)
+            fp_names = os.path.join(os.path.dirname(__file__), name_file)
 
-            fp_items = os.path.join(os.path.dirname(__file__), fp_items)
-            fp_names = os.path.join(os.path.dirname(__file__), fp_names)
+            items = []
+            names = []
 
             with open(fp_items) as json_file:
                 temp_items = json.load(json_file)
-                self._items.extend(temp_items['items']['equipmentitem'])
-                self._items.extend(temp_items['items']['weapon'])
-                self._items.extend(temp_items['items']['mount'])
-                assert self._items is not None, "Failed to load items"
+                items.extend(temp_items['items']['equipmentitem'])
+                items.extend(temp_items['items']['weapon'])
+                items.extend(temp_items['items']['mount'])
+                assert items is not None, "Failed to load items"
 
             with open(fp_names, encoding='utf8') as json_file:
-                self._names = json.load(json_file)
-                self._names = [x for x in self._names if x['LocalizedNames'] != None]
-                assert self._names is not None, "Failed to load item names"
+                names = json.load(json_file)
+                names = [x for x in names if x['LocalizedNames'] != None]
+                assert names is not None, "Failed to load item names"
 
-            self._map_item_names()
+            self._map_item_names(items, names)
 
         except Exception as e:
             print(f'Failed to create: {type(self).__name__}')
@@ -49,27 +56,15 @@ class AoBinUtils():
             i = i + 1
 
     
-    def _map_item_names(self):
+    def _map_item_names(self, items, names):
         res = {}
         try:
-            del_indeces = []
-            index = 0
-            for item in self._items:
-                item_name = [x['LocalizedNames']['EN-US'] for x in self._names if x['UniqueName'] == item['@uniquename']]
+            for item in items:
+                item_name = [x['LocalizedNames']['EN-US'] for x in names if x['UniqueName'] == item['@uniquename']]
                 if len(item_name) == 1:
                     res[item_name[0]] = item                     
-                else:
-                    del_indeces.append(index)
-
-                index = index + 1
-
-            i_offset = 0
-            for i in del_indeces:
-                del self._items[i - i_offset]
-                i_offset = i_offset + 1
 
             assert len(res) > 0, 'Res is empty'
-            assert len(self._items) == len(res), 'Res and _items size differ'
 
             self._item_name = res
         
@@ -80,6 +75,6 @@ class AoBinUtils():
 
 
 if __name__ == "__main__":
-    abu = AoBinUtils(item_file='..\\items.json', name_file='..\\formatted\\items.json')
+    abu = AoBinUtils()
 
     abu.print_head()
