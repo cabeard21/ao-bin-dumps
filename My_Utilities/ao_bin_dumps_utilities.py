67ea0db144476f5580ba7607abed1c4ef682f456
DESKTOP-ENBUIB3\cabeard21 <cabeard21@gmail.com>
2020-12-10 01:52:03 -0600

    Refactor singleton meta for thread-safety.

--- My_Utilities/ao_bin_dumps_utilities.py
+++ My_Utilities/ao_bin_dumps_utilities.py
@@ -1,15 +1,18 @@
 import json
 import os  # For relative paths
+from threading import Lock # For Singleton
 
 
 class SingletonMeta(type):
 
     _instances = {}
+    _lock: Lock = Lock()
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
 
         return cls._instances[cls]
 
