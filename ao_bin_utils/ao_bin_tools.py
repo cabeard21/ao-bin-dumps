c86fed3b90b86d558107db9c5f3bb44a5e27175c
DESKTOP-ENBUIB3\cabeard21 <cabeard21@gmail.com>
2020-12-11 15:32:20 -0600

    Added lots of comments to ao_bin_tools.py.

--- ao_bin_utils/ao_bin_tools.py
+++ ao_bin_utils/ao_bin_tools.py
@@ -7,12 +7,38 @@ from __future__ import annotations
 
 
 class AoBinTools():
+    """Allows for calculations to be perfomed on AO Bin data.
+
+    Different Strategies can be used on AO Bin data by changing the Strategy attribute.
+    
+    ...
+
+    Attributes
+    ----------
+    _strategy: Strategy object
+        The Strategy that will perform an operation on AO Bin data.
+    _ao_data: AoBinData object
+        The data to be accessed by Strategy.
+
+    Methods
+    -------
+    get_calculation():
+        Calls the Strategy's algorithm and passes _ao_data to it.
+    """
 
     def __init__(self, strategy: Strategy):
+        """Constructor that takes a Strategy
+        
+        Parameters
+        ----------
+        strategy: Strategy
+            A concrete implementation of the Strategy abstract class. Performs the actual calculation.
+        """
         self._strategy = strategy
         self._ao_data = AoBinData()
 
 
+    # Getter and setter for strategy.
     @property
     def strategy(self) -> Strategy:
         return self._strategy
@@ -24,25 +50,113 @@ class AoBinTools():
 
 
     def get_calculation(self) -> Dict:
+        """Calls the Strategy's algorithm and passes _ao_data to it.
+
+        Returns
+        -------
+        dictionary
+            A dictionary with keys and values that depend on the stratgey used.
+        """
+
         return self._strategy.algorithm(self._ao_data)
 
 
 
 class Strategy(ABC):
+    """Abstract Strategy base class.
+    
+    ...
+
+    Methods
+    -------
+    algorithm(ao_data):
+        Abstract method that is used by client to call concrete Strategy algorithm.
+    """
 
     @abstractmethod
-    def algorithm(self, ao_data) -> Dict:
+    def algorithm(self, ao_data: AoBinData) -> Dict:
+        """Abstract method that is used by client to call concrete Strategy algorithm.
+
+        Parameters
+        ----------
+        ao_data: AoBinData object
+            Allows the concrete class to access item data. Provided by the context class to avoid tight coupling.
+
+        Returns
+        -------
+        dictionary
+            A dictionary with keys and values that depend on the stratgey used.
+        """
+
         pass
 
 
 
 class EfficientItemPower(Strategy):
-    """Strategy to return the cheapest items that are at least a specified IP"""
+    """Strategy to return the cheapest items that are at least a specified IP.
+    
+    The idea is to find the cheapest version of each item in a build that is at least
+    a minimum Item Power (IP).
 
-    def __init__(self, target_ip, items: List):
+    ...
+
+    Attributes
+    ----------
+    target_ip: int
+        The minimum IP each item must be.
+    items: list
+        A list of item unique names that represent the build.
+    specs: list
+        A list of integers that are the bonus IP for each item.
+    location: str
+        The name of the market to use
+
+    Methods
+    -------
+    algorithm(ao_data)
+        Concrete implementation of the abstract method inherited from Strategy.
+    """
+
+    def __init__(self, target_ip: int, items: List, specs: List, location: str):
+        """Constructor for the class.
+
+        Parameters
+        ----------
+        target_ip: int
+            The minimum IP each item must be.
+        items: list
+            A list of item unique names that represent the build.
+        specs: list
+            A list of integers that are the bonus IP for each item.
+        location: str
+            The name of the market to use
+        """
+
         self._target_ip = target_ip
         self._items = items
+        self._location = location
 
     
-    def algorithm(self, ao_data) -> Dict:
+    def algorithm(self, ao_data: AoBinData) -> Dict:
+        """Concrete implementation of the abstract method inherited from Strategy.
+        
+        The method accomplishes the class's goal by:
+            1: Loop through each item stored by the class.
+            2: For each item:
+                a: Use ao_data to find all items that meet the minimum IP when including specs.
+                b: Do an API call to get the price for all items found in (a).
+                c: Add the cheapest item and price to the result dictionary.
+
+        Parameters
+        ----------
+        ao_data: AoBinData object
+            Allows the concrete class to access item data. Provided by the context class to avoid tight coupling.
+
+        Returns
+        -------
+        dictionary
+            'Items': List of item unique names for the chosen items.
+            'Prices': List of floats that are the market prices for each item in 'Items'.
+        """
+
         pass
