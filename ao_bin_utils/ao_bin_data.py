d2e2456d839cb809094b86b2033d4e05fdb176fa
DESKTOP-ENBUIB3\cabeard21 <cabeard21@gmail.com>
2020-12-14 13:42:06 -0600

    Commenting for ao_bin_data.py and ao_bin_utilities.py.

--- ao_bin_utils/ao_bin_data.py
+++ ao_bin_utils/ao_bin_data.py
@@ -4,11 +4,34 @@ from threading import Lock # For Singleton
 
 
 class SingletonMeta(type):
+    """Abstract base class used for the Singleton design pattern.
+
+    Attributes
+    ----------
+    _instanced: dictionary
+        Dictionary that stores each class' singleton instance.
+    _lock: Lock object
+        Locks access to the dictionary of instances when a thread is accessing it.
+
+    Methods
+    -------
+    __call__(cls, *args, **kwargs):
+        Returns a pointer to the single instance of the class.
+
+        This is called whenever an object is created. It will create an instance of the class
+        if it isn't found, otherwise it returns the previously created instance. This is thread-safe.
+    """
 
     _instances = {}
     _lock: Lock = Lock()
 
     def __call__(cls, *args, **kwargs):
+        """Returns a pointer to the single instance of the class.
+
+        This is called whenever an object is created. It will create an instance of the class
+        if it isn't found, otherwise it returns the previously created instance. This is thread-safe.
+        """
+
         with cls._lock:
             if cls not in cls._instances:
                 instance = super().__call__(*args, **kwargs)
@@ -19,10 +42,47 @@ class SingletonMeta(type):
 
 
 class AoBinData(metaclass=SingletonMeta):
+    """Represents the AO Data from the game's extracted binaries.
+
+    This should act as the primary interface to AO data.
+
+    ...
+
+    Properties
+    ----------
+    _item_name: dictionary
+        Dictionary of "readable" item names and it's JSON data.
+
+    Methods
+    -------
+    __init__(self, item_file, name_file)
+        Constructor sets location of relevant data files.
+    print_head(self, n)
+        Prints the first n items of _item_name.
+    _map_item_names(self, items, names)
+        Builds the _item_name dictionary.
+    """
 
     _item_name = {}
 
     def __init__(self, item_file='..\\items.json', name_file='..\\formatted\\items.json'):
+        """Constructor sets location of relevant data files.
+
+        Performs any additional initialization e.g. building complex class attributes.
+
+        Parameters
+        ----------
+        item_file: str
+            Location of JSON file containing item data.
+        name_file: str
+            Location of JSON file containing localization data for items.
+
+        Raises
+        ------
+        Assertion Error
+            If any file location is incorrect or fails to load.
+        """
+
         try:
             fp_items = os.path.join(os.path.dirname(__file__), item_file)
             fp_names = os.path.join(os.path.dirname(__file__), name_file)
@@ -51,6 +111,14 @@ class AoBinData(metaclass=SingletonMeta):
 
 
     def print_head(self, n=5):
+        """Prints the first n items of _item_name.
+
+        Parameters
+        ----------
+        n: int (Default: 5)
+            The number of items to print.
+        """
+
         i = 0
         for name, item in self._item_name.items():
             if i >= n:
@@ -61,6 +129,23 @@ class AoBinData(metaclass=SingletonMeta):
 
     
     def _map_item_names(self, items, names):
+        """Builds the _item_name dictionary.
+
+        Sets the keys to the item's localized name. Sets the value to the JSON data of the item.
+
+        Parameters
+        ----------
+        items: JSON object
+            Item data from the forked AO Binary Data repo.
+        names: JSON object
+            Localization data for the items from the forked AO Binary Data repo.
+
+        Raises
+        ------
+        Assertion Error
+            If there are no item/name pairs found in the given parameters.
+        """
+        
         res = {}
         try:
             for item in items:
--- ao_bin_utils/ao_bin_utilities.py
+++ ao_bin_utils/ao_bin_utilities.py
@@ -1,8 +1,36 @@
 
 
 def get_item_price(item_unique_name, location) -> float:
+    """Utility function to get an item's cheapest sell price at a given location.
+
+    Parameters
+    ----------
+    item_unique_name: str
+        Unique name of the item to be found.
+    location: str
+        Name of the market whose price should be used.
+
+    Returns
+    -------
+    float
+        Cheapest sell price found at the location for the item.
+    """
     pass
 
 
 def get_item_power(item_unique_name, ao_data) -> float:
+    """Utility function to find an item's Item Power.
+
+    Parameters
+    ----------
+    item_unique_name: str
+        Unique name of the item to be found.
+    ao_data: AoBinData object
+        Pointer to the AoBinData object containing item information.
+
+    Returns
+    -------
+    float
+        The item's Item Power.
+    """
     pass
