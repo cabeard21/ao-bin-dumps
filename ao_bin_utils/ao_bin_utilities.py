a39f7f21bb554e60ee6f0570377a9344fb81218a
DESKTOP-ENBUIB3\cabeard21 <cabeard21@gmail.com>
2021-01-28 21:26:56 -0600

    Update ao_bin_utilities.py

--- ao_bin_utils/ao_bin_utilities.py
+++ ao_bin_utils/ao_bin_utilities.py
@@ -76,14 +76,14 @@ def get_item_price(item_unique_name, quality, location) -> List:
         for item_index in range(len(names)):
             item_index = item_index - item_index_offset
             for item in response:
-                # data_age = (
-                #     datetime.now() - datetime.strptime(item['sell_price_min_date'], '%Y-%m-%dT%H:%M:%S')
-                # )
+                data_age = (
+                    datetime.now() - datetime.strptime(item['sell_price_min_date'], '%Y-%m-%dT%H:%M:%S')
+                )
                 if (
                     names[item_index] == item['item_id'] and
                     quality_copy[item_index] == item['quality'] and
-                    item['sell_price_min'] > 0 # and
-                    # data_age.days <= 1
+                    item['sell_price_min'] > 0 and
+                    data_age.days <= 1
                 ):
                     res.append(
                         (
